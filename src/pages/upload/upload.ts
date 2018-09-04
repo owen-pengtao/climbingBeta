4101458e8fde3e2f901de4ab864253b9710ff3b9
Owen Peng <tpeng@tibco.com>
2018-09-04 10:46:06 -0500

    change primary color to yellow, add upload form

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +7,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'upload.html'
 })
 export class UploadPage {
+  isVideoUrl: boolean = false;
+  uploadForm: FormGroup;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+    this.uploadForm = formBuilder.group({
+      zone: ['1'],
+      route: ['2'],
+      performer: ['Owen'],
+      videoPath: [''],
+      videoUrl: [''],
+      tips: [''],
+      contacts: ['']
+    });
   }
 
+  switchVideoSource(bool: boolean) {
+    this.isVideoUrl = !bool;
+    console.log(this.isVideoUrl);
+  }
+
+  upload() {
+
+  }
 }
